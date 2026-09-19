99cb6f12649970c5146a2e7fc8a3c39164ad3a89
OscarMorales18 <omoralesl7@miumg.edu.gt>
2026-09-19 11:23:14 -0600

    Agregando reporte a los usuarios

---
+++ CapacitacionMVC(FASEII)/CapaVista_CapacitacionMVC(FASEII)/Reportes/frmReporteUsuarios.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador_CapacitacionMVC_FASEII_.Modelos;
+
+namespace CapaVista_CapacitacionMVC_FASEII_.Reportes
+{
+    public partial class frmReporteUsuarios : Form
+    {
+        private ModeloUsuarios usuarios = new ModeloUsuarios();
+        public frmReporteUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReporteUsuarios_Load(object sender, EventArgs e)
+        {
+            ReportDataSource reportDataSource = new ReportDataSource("ReporteUsuarios", usuarios.GetAll());
+            reportViewer1.LocalReport.ReportEmbeddedResource = "CapaVista_CapacitacionMVC_FASEII_.Reportes.ReporteUsuarios.rdlc";
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
